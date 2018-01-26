df9a3d2eab2f6b4464a28faadb7f3214d3fa7f36
Colin MJ <colinmj@telus.net>
2018-01-26 14:22:44 -0800

    Created Item Mutation

--- src/containers/Items/Items.js
+++ src/containers/Items/Items.js
@@ -1,42 +1,42 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import ItemCard from '../../components/ItemCard/ItemCard';
-import Masonry from 'react-masonry-component';
-import Styles from './styles.js';
-import Style from './style.css';
-
-
+import React from "react";
+import PropTypes from "prop-types";
+import ItemCard from "../../components/ItemCard/ItemCard";
+import Masonry from "react-masonry-component";
+import Styles from "./styles.js";
+import Style from "./style.css";
 
 const masonryOptions = {
   transitionDuration: 2000
 };
 
-const Items = ({list, owner}) => ( //******/
-  <div 
-  /* style={Styles.ListContainer} */
-  className={'list-container'}>
-    
-    <Masonry
-    options={masonryOptions}
-    elementType={'ul'}
-    >
-     {list.map(item => <li 
-     /* style={Styles.SingleItem} */
-     key={item.id}
-     className={'single-item'}>
-    <ItemCard style={Styles.ItemCard} item={item} owner={item.itemowner.id} key={item.id}/></li>)}
+const Items = (
+  { list, owner } //******/
+) => (
+  <div>
+    <Masonry options={masonryOptions} elementType={"ul"}>
+      {list &&
+        list.map(item => (
+          <li
+            /* style={Styles.SingleItem} */
+            key={item.id}
+            className={"single-item"}
+          >
+            <ItemCard
+              style={Styles.ItemCard}
+              item={item}
+              owner={item.itemowner.id}
+              key={item.id}
+            />
+          </li>
+        ))}
     </Masonry>
   </div>
 );
 
-
 Items.propTypes = {
   list: PropTypes.array.isRequired
-}
+};
 
 export default Items;
 
 // this is a function that react calls when it renders
-
-
-
--- src/containers/Items/ItemsContainer.js
+++ src/containers/Items/ItemsContainer.js
@@ -3,43 +3,86 @@ import Items from "./Items";
 import PropTypes from "prop-types";
 import { fetchItemsAndUsers } from "../../redux/modules/items";
 import { connect } from "react-redux";
+import { graphql } from "react-apollo";
+import gql from "graphql-tag";
 
 class ItemsContainer extends Component {
-  static propTypes = {}; // ??
+  // static propTypes = {}; // ??
+  // propTypes = {
+  //   loading: PropTypes.bool,
+  //   items: PropTypes.array
+  // };
 
-  componentDidMount() {
-    this.props.dispatch(fetchItemsAndUsers());
-  }
+  // componentDidMount() {}
+  // this.props.dispatch(fetchItemsAndUsers());
+
+  // const ItemsList = ({ data }) => (
+  //   <ul>{data.items.map(({ title }, i) => <li key={i}>{title}</li>)}</ul>
+  // );
+  // const fetchItems = gql`
+  //   query fetchMovies {
+  //     items {
+  //       title
+  //     }
+  //   }
+  // `;
 
   render() {
-    if (this.props.isLoading) return <p>Loading</p>;
+    // if (this.props.isLoading) return <p>Loading</p>;
 
-    const filtered = this.props.items.filter(item => {
-      return item.tags.some(tag => {
-        return this.props.selectedTags.includes(tag.title);
-      });
-    });
+    // const filtered = this.props.items.filter(item => {
+    //   return item.tags.some(tag => {
+    //     return this.props.selectedTags.includes(tag.title);
+    //   });
+    // });
+    {
+      /* this.props.selectedTags.length === 0 ? this.props.items : filtered */
+    }
 
-    console.log(filtered);
+    const { loading, items } = this.props.data;
+    console.log(items);
 
-    return (
-      <Items
-        list={
-          this.props.selectedTags.length === 0 ? this.props.items : filtered
-        }
-      />
-    );
+    // console.log(filtered);
+
+    return loading ? <p> Loading</p> : <Items list={items} />;
 
     // return <div>hi</div>
   }
 }
 
-const mapStateToProps = state => ({
-  isLoading: state.items.isLoading,
-  items: state.items.items,
-  selectedTags: state.items.selectedTags,
-  error: state.items.error,
-  itemsFilter: state.items.itemsFilter
-});
+const fetchItems = gql`
+  query {
+    items {
+      id
+      title
+      itemowner {
+        id
+        email
+        fullname
+      }
+      borrower {
+        id
+        fullname
+      }
+      created
+      imageurl
+      description
+      available
+      tags {
+        id
+        title
+      }
+    }
+  }
+`;
 
-export default connect(mapStateToProps)(ItemsContainer);
+// const mapStateToProps = state => ({
+//   isLoading: state.items.isLoading,
+//   items: state.items.items,
+//   selectedTags: state.items.selectedTags,
+//   error: state.items.error,
+//   itemsFilter: state.items.itemsFilter
+// });
+
+// export default connect(mapStateToProps)(ItemsContainer);
+export default graphql(fetchItems)(ItemsContainer);
